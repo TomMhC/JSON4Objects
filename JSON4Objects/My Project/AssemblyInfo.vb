b3b3f19ae44c9309bc3c766dfc7780392cecf098
Tom Cloos <tomcloos@hotmail.com>
2017-02-10 15:36:45 +0100

    Updated assembly information

--- JSON4Objects/My Project/AssemblyInfo.vb
+++ JSON4Objects/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("JSON2Objects")> 
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyTitle("JSON4Objects")> 
+<Assembly: AssemblyDescription("JSON4Objects is a complete JSON serializer written in VB.Net which allows deep, complex serialization")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("JSON2Objects")> 
-<Assembly: AssemblyCopyright("Copyright ©  2011")> 
+<Assembly: AssemblyProduct("JSON4Objects")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.1.0.0")> 
+<Assembly: AssemblyFileVersion("1.1.0.0")> 
