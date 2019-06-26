e58b5098cd1ba64c4552bd1b9772897350461e83
Tom Cloos <tomcloos@hotmail.com>
2019-06-26 12:02:20 +0200

    Updated assembly version

--- JSON4Objects/My Project/AssemblyInfo.vb
+++ JSON4Objects/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("b0f8e970-abab-4aed-982e-875dd42641a4")> 
+<Assembly: Guid("b0f8e970-abab-4aed-982e-875dd42641a4")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.0")> 
-<Assembly: AssemblyFileVersion("1.2.0.0")> 
+<Assembly: AssemblyVersion("1.3.0.0")>
+<Assembly: AssemblyFileVersion("1.3.0.0")>
